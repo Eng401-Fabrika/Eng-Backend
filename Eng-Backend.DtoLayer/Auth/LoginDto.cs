40b163cc7b0780365163ad541543469c820dfba1
altugmalkan <altug33@windowslive.com>
2025-12-01 11:10:02 +0300

    feat: add validation attributes to authdto's

--- Eng-Backend.DtoLayer/Auth/LoginDto.cs
+++ Eng-Backend.DtoLayer/Auth/LoginDto.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Eng_Backend.DtoLayer.Auth;
 
 public class LoginDto
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
     public string Password { get; set; }
 }
